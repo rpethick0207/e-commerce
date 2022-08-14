9adac6ec8b2f564de0ffaed31aa6f06ac77591a4
rpethick0207 <101664398+rpethick0207@users.noreply.github.com>
2022-08-13 20:40:05 -0400

    user log in

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ?  new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+      host: process.env.DB_HOST,
       username: process.env.DB_USER,
       password: process.env.DB_PW,
       database: process.env.DB,
