49d25fe334f71f09dc0758a3624c9f4439608f47
Ricky Pethick <rpethick0207@gmail.com>
2022-08-14 19:52:25 -0400

    user

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const apiRoutes = require('../controllers/api');
 const homeRoutes = require('../controllers/api/home-routes');
 
+
 router.use('/', homeRoutes);
+
 router.use('/api', apiRoutes);
 
 module.exports = router;
